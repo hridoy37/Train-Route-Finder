3f2dd14656aa87c8ea264be312c50352fd71a68f
hridoy37 <58269572+hridoy37@users.noreply.github.com>
2024-07-10 11:02:41 +0600

    creating admin panel

--- Finder/admin.py
+++ Finder/admin.py
@@ -1,1 +1,20 @@
+from django.contrib import admin
+from .models import Train, Station, TrainRoute
 
+class TrainAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class StationAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class TrainRouteAdmin(admin.ModelAdmin):
+    list_display = ('train', 'from_station', 'to_station', 'departure_time', 'arrival_time', 'fare')
+    list_filter = ('train', 'from_station', 'to_station')
+    search_fields = ('train__name', 'from_station__name', 'to_station__name')
+
+# Register your models here.
+admin.site.register(Train, TrainAdmin)
+admin.site.register(Station, StationAdmin)
+admin.site.register(TrainRoute, TrainRouteAdmin)
